78ca23e1928451d3536e6e62fe9bf1d052547570
jammcc <jamie@smarteragent.com>
2020-06-08 15:04:36 -0400

    leave out special characters

--- name_dl_script.py
+++ name_dl_script.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 # Script to scrape a website for a bunch of names
 
@@ -14,7 +15,7 @@ soup = BeautifulSoup(r.text, 'html.parser')
 names = {}
 
 for p in people:
-    names[p] = {'masc': [], 'fem': [], 'surname': []}
+    names[p] = {'masc': [], 'fem': [], 'surnames': []}
     for x in range(1, 5):
         print(f'Fetching {url.format(p, x)}')
         r = requests.get(url.format(p, x))
@@ -22,15 +23,24 @@ for p in people:
         list_names = soup.findAll("span", {"class": "listname"})
         for n in list_names:
             gender = 'fem' if n.parent.find("span", {"class": "fem"}) else 'masc'
-            names[p][gender].append(n.contents[0].text)
+            na = n.contents[0].text.split(' ')
+            # filter out duplicates. Also filter out special characters because they display badly, dunno how to fix
+            if len(na) > 1 and na[1] != '(1)':
+                continue
+            if '\\' in json.dumps(na[0]):
+                continue
+            na = na[0]
+            names[p][gender].append(na)
         if not list_names:
             break
     for x in range(1, 5):
-        print(f'Fetching {url.format(p, x)}')
+        print(f'Fetching {s_url.format(p, x)}')
         r = requests.get(s_url.format(p, x))
         soup = BeautifulSoup(r.text, 'html.parser')
         list_names = soup.findAll("span", {"class": "listname"})
         for n in list_names:
-            names[p]['surname'].append(n.contents[0].text)
+            names[p]['surnames'].append(n.contents[0].text)
         if not list_names:
             break
+
+json.dumps(names)
